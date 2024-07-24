9ae231fa4c25de432d17be88a35faffca32a1055
Mephisto Lin <Mephisto007linlin@outlook.com>
2024-07-24 20:36:21 +0800

    feat: update Ch2-3 codes

--- src/Ch2/Ch2_3.sql
+++ src/Ch2/Ch2_3.sql
@@ -6,3 +6,12 @@ from
 	product
 where
 	product_name = '藥用手皂';
+
+
+-- 字串進階
+
+select * from search where val = 'A';
+
+select * from search where val COLLATE Latin1_General_BIN2 = 'A';
+
+select * from product where product_name like '藥用%'
