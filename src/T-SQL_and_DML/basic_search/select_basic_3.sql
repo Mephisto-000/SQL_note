1f967443575f80156bd7597ddfd3e6e4d3855332
Mephisto Lin <Mephisto007linlin@outlook.com>
2024-07-28 16:57:56 +0800

    feat: add with ties practice

--- src/T-SQL_and_DML/basic_search/select_basic_3.sql
+++ src/T-SQL_and_DML/basic_search/select_basic_3.sql
@@ -14,3 +14,9 @@ SELECT TOP 3 * FROM 學生
 ---- 取出 學生 前 25 % 的學生記錄
 SELECT TOP 25 PERCENT * FROM 學生
 
+-- WITH TIES 與 ORDER BY 子句
+---- 利用 WITH TIES 取出 課表 資料表內前 3 筆資料，如有同學分的記錄也一併顯示出來
+---- 並且利用 ORDER BY 子句指定用 學分 來做排序規則
+SELECT TOP 3 WITH TIES * FROM 課程
+	ORDER BY 學分
+
